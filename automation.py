32eed8113e06c2a2d960c900b895749722e51802
summersunshine1 <summersunshine366@gmail.com>
2016-12-24 19:19:10 +0800

    add automation execution code

---
+++ automation.py
@@ -0,0 +1,8 @@
+import os
+os.system('F:/course/sentimentcode/feature/removestop.py')
+os.system('F:/course/sentimentcode/feature/chunkreview.py')
+os.system('F:/course/sentimentcode/feature/formatbasket.py')
+os.system('F:/course/sentimentcode/feature/findfeatures.py')
+os.system('F:/course/sentimentcode/feature/getnewfeatures.py')
+os.system('F:/course/sentimentcode/feature/opinionextraction.py')
+
